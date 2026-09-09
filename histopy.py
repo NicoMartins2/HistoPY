9c731193fa27b10805658254fc810262379f5c30
NicoMartins2 <nick.legal@outlook.com>
2026-09-08 23:13:37 -0300

    +win file creates if doesnt exist, +id def (not finished)

--- histopy.py
+++ histopy.py
@@ -4,6 +4,9 @@ from pathlib import Path
 def get_os():
     usr_os = os.name
     if usr_os == 'nt':
+        history_dir = Path.home() / 'AppData' / 'Roaming' / 'Microsoft' / 'Windows' / 'PowerShell' / 'PSReadLine' / 'ConsoleHost_history.txt'
+        if not history_dir.exists():
+            Path.mkdir(history_dir, parents=True) # create the directory if it doesn't exist
         return win
     elif usr_os == 'linux':
         return linux
@@ -15,9 +18,11 @@ if __name__ == "__main__":
         raise Exception("Invalid number of arguments provided. Please provide exactly one argument.")
     else:
         os_module = get_os()
-        if sys.argv[1] == "getall":
-            os_module.getall()
-        elif sys.argv[1] == "getlast":
-            os_module.getlast()
+        if sys.argv[1] == "all":
+            os_module.all()
+        elif sys.argv[1] == "last":
+            os_module.last()
+        elif sys.argv[1] == "id":
+            os_module.id()
         else:
             raise Exception("Invalid argument provided.")
--- win.py
+++ win.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 history_dir = Path.home() / 'AppData' / 'Roaming' / 'Microsoft' / 'Windows' / 'PowerShell' / 'PSReadLine' / 'ConsoleHost_history.txt'
 
-def getall():
+def all():
     with open(history_dir, 'r', encoding='utf-8') as file:
         history = file.readlines()
         command_line_number = 0
@@ -12,8 +12,17 @@ def getall():
             command = history[i].strip()
             print(f"{command_line_number}: {command}")
 
-def getlast():
+def last():
     with open(history_dir, 'r', encoding='utf-8') as file:
         history = file.readlines()
         last_command = history[-1].strip()
         print(f"Last command: {last_command}")
+
+def id():
+    with open(history_dir, 'r', encoding='utf-8') as file:
+        history = file.readlines()
+        command_line_number = 0
+        for i in range(len(history)):
+            command_line_number += 1
+            command = history[i].strip()
+            print(f"{command_line_number}: {command}")
